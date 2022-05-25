83266c443dab8b6307836f49be990e496369d144
HoangLong <hoanglong15022k@gmail.com>
2022-05-26 02:06:35 +0700

    built layout tiktok

--- src/components/Layout/DefaultLayouts/index.js
+++ src/components/Layout/DefaultLayouts/index.js
@@ -1,12 +1,15 @@
 import Header from '~/components/Layout/components/Header';
 import Siderbar from './Siderbar';
+import classNames from 'classnames/bind';
+import styles from './DefaultLayouts.module.scss';
+const cx = classNames.bind(styles);
 function DefaultLayouts({ children }) {
   return (
-    <div>
+    <div className={cx('wrapper')}>
       <Header />
-      <div className="container">
+      <div className={cx('container')}>
         <Siderbar />
-        <div className="content">{children}</div>
+        <div className={cx('content')}>{children}</div>
       </div>
     </div>
   );
